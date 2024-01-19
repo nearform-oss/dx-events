f9e3775e576795b29844369055422b3c3328e1ae
Cody Zuschlag <cody.zuschlag@gmail.com>
2024-01-19 15:03:16 +0100

    inject favicons + add meta tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import unoCSS from 'unocss/vite';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), unoCSS()],
+  plugins: [
+    react(),
+    unoCSS(),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    vitePluginFaviconsInject('./public/nf.svg', {
+      appName:
+        'DX Events Visualizer: Visual explorer of Nearform DX events from 2023',
+      appShortName: 'DX Events Visualizer',
+      appDescription:
+        "Provides an interactive map of all of the Nearform DX Team's events from the year 2023",
+      // Extra options...
+    }),
+  ],
 });
